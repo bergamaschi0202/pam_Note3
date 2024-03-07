9eb3287416ff6ff36289567f760ac937189b594b
Arthur Bergamaschi <arthurbergamaschi8@gmail.com>
2024-03-07 12:37:54 -0300

    Isso ´´é para vc

---
+++ pam_Note3/pam_Note3/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace pam_Note3;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ pam_Note3/pam_Note3/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace pam_Note3
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
